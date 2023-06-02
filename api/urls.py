b603255fa3768dc9ce2847970ae331c00ed0a3e9
Renat Azibaev <37597315+Slond@users.noreply.github.com>
2023-06-02 07:00:36 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('resume/', views.ResumeList.as_view()),
-    path('resume/<int:pk>/', views.ResumeDetail.as_view()),
+    path('', views.ResumeList.as_view()),
+    path('<int:pk>', views.ResumeDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
